c5db05aac7c04569cd7b3c7e53395a8f7b99dc9f
Márcio Júnior <marcio.mj70@gmail.com>
2021-06-24 02:01:34 -0300

    day 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { Button } from "./components/Button";
+import { BrowserRouter, Route } from 'react-router-dom';
+import { AuthContextProvider } from './contexts/AuthContext';
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <div>
-      <Button />
-      <Button />
-      <Button />
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
